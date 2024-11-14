db6e74de95f251e2efc4f1b738a3351afad4d542
Mangyec2009 <amin.tj2010@gmail.com>
2024-11-14 18:52:21 +0500

    usmon ataka titanov tamosho mekna

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Loading from '../../shared/Loading/Loading'
+import { useTranslation } from 'react-i18next';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import "@/i18n";
 
 const Home = () => {
   return <>
     <div  className="">Home</div>
-    
   </>
 }
 
--- src/shared/Layout/Layout.jsx
+++ src/shared/Layout/Layout.jsx
@@ -4,18 +4,37 @@ import SearchBar from '../Search/Bar';
 import PhoneIcon from '@mui/icons-material/Phone';
 import { ThemeProvider } from '@/components/theme-provider';
 import { ModeToggle } from '@/components/mode-toogle';
+import { useTranslation } from 'react-i18next';
+import "@/i18n";
 
 const Layout = () => {
+  const {t, i18n} = useTranslation();
+  function TranslateClick(lang) {
+      i18n.changeLanguage(lang);
+  }
   return <>
   <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <div className="max-w-[1280px] m-auto py-[20px] px-[10px] flex items-center justify-between ">
-      <Link to={"/"}><img src="public/logo.png" className="w-[150px]" /></Link>
+      <Link to={"/"}><img src="public/lgo.png" className="w-[150px]" /></Link>
         <SearchBar />
         <PhoneIcon />
         <div className=" flex items-center gap-[20px]">
-          <Link to={"/about"}>About</Link>
+          <Link to={"/about"}>
+            <h1>{t('Example.1')}</h1>
+          </Link>
         </div>
         <ModeToggle  />
+        <select>
+          <option value="">
+            <button onClick={()=>TranslateClick('en')}>en</button>
+          </option>
+          <option>
+            <button onClick={()=>TranslateClick('ru')}>ru</button>
+          </option>
+          <option value="">
+            <button onClick={()=>TranslateClick('tj')}>tj</button>
+          </option>
+        </select>
     </div>
     <div className='max-w-[1280px] m-auto'>
         <Outlet></Outlet>
